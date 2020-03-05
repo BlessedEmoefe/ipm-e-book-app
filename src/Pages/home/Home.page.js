0e68170e44884ff577e7b71ba003f61bfc7ffd90
BlessedEmoefe <blessedemoefe1122334455@gmail.com>
2020-03-05 23:32:15 +0100

    i just worked on the header and the footer

--- src/Pages/home/Home.page.js
+++ src/Pages/home/Home.page.js
@@ -2,13 +2,21 @@ import React, { Component } from "react";
 import styles from "./Home.module.css";
 import Header from "../../component/Header/Header";
 import Footer from "../../component/footer/Footer";
+// import books from "../../assets/books.webp"
 
 class Home extends Component {
   render() {
     return (
-      <div className={styles.initialHeader}>
+      <div className={styles.homeHeader}>
         <Header />
-        <p className="paragragh">Home</p>
+        <div className={styles.container}>
+          <div className={styles.homeSlider}>
+        
+          </div>
+          <div className={styles.second}></div>
+          <div className={styles.third}></div>
+          <div className={styles.fourth}></div>
+        </div>
         <Footer />
       </div>
     );
--- src/component/footer/Footer.js
+++ src/component/footer/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 import styles from "./Footer.module.css";
 
 const Footer = () => {
-  return <div className={styles.footer}>Footer</div>;
+  return <div className={styles.footer}>Copyright© IPM 2020 || Powered By Inovation Tech Terrain</div>;
 };
 
 export default Footer;

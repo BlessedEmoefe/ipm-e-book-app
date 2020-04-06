57a74b86a027e0dd7a1c7ad82c56ec2dc5e581bc
BlessedEmoefe <blessedemoefe1122334455@gmail.com>
2020-04-06 11:55:12 +0200

    made some additional build

--- src/Pages/home/Home.page.js
+++ src/Pages/home/Home.page.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import styles from "./Home.module.css";
 import Header from "../../component/Header/Header";
 import Footer from "../../component/footer/Footer";
-// import books from "../../assets/books.webp"
+import books from "../../assets/books.png";
 
 class Home extends Component {
   render() {
@@ -10,12 +10,33 @@ class Home extends Component {
       <div className={styles.homeHeader}>
         <Header />
         <div className={styles.container}>
-          <div className={styles.homeSlider}>
-        
+          <div className={styles.homeFirstSection}>
+            <div className={styles.LandingPagecontent}>
+              <div className={styles.landingText}>
+                <p className={styles.text1}>The Book Shelf</p>
+                <p className={styles.text2}>Checkout Our Library</p>
+                <button className={styles.button}>Get A Book</button>
+              </div>
+              <img className={styles.landingImage} src={books} />
+            </div>
           </div>
-          <div className={styles.second}></div>
-          <div className={styles.third}></div>
-          <div className={styles.fourth}></div>
+
+          <div className={styles.homeSecondSection}>
+            <title className={styles.title}>What is IPM?</title>
+            <div className={styles.content}></div>
+          </div>
+          <div className={styles.homeThirdSection}>
+            <title className={styles.title}>Books</title>
+            <div className={styles.content}></div>
+          </div>
+          <div className={styles.homeFourthSection}>
+            <title className={styles.title}>News</title>
+            <div className={styles.content}></div>
+          </div>
+          <div className={styles.homeFifthSection}>
+            <title className={styles.title}>Support Us</title>
+            <div className={styles.content}></div>
+          </div>
         </div>
         <Footer />
       </div>
